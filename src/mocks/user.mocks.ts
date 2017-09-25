8c079dc2ea2383ca9653daaeee8845d474182593
Sharad Shinde <shindesharad71@gmail.com>
2017-09-25 16:21:25 +0530

    demo data for app testing created

---
+++ src/mocks/user.mocks.ts
@@ -0,0 +1,14 @@
+import { User } from '../models/user.interface';
+
+const userList: User[] = [
+  {
+    name: 'Sharz',
+    company: 'ABC Company',
+    location: 'Pune India',
+    bio: 'Web Developer',
+    avatar_url: 'http://google.com/logo.png',
+    email: 'shindesharad71@gmail.com'
+  }
+];
+
+export const USER_LIST = userList;
